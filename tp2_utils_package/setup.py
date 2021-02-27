75835b7e27fcd856479bbcbb1ce33753b4dcea2c
Gianmarco Cafferata <giancafferata@hotmail.com>
2021-02-27 20:04:00 -0300

    Forgot numpy in requirements

--- tp2_utils_package/setup.py
+++ tp2_utils_package/setup.py
@@ -7,5 +7,6 @@ setuptools.setup(
     author_email="giancafferata@hotmail.com",
     packages=setuptools.find_packages(),
     python_requires='>=3.8',
-    install_requires=['pika==1.1.0', 'wheel==0.35.1', 'pyhash==0.9.3', 'dill==0.3.3']
+    install_requires=['pika==1.1.0', 'wheel==0.35.1',
+                      'pyhash==0.9.3', 'dill==0.3.3', 'numpy==1.20.1']
 )
